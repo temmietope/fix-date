4f1e2eac05f052aa41d91677cd6108a69b52dbcb
Temitope <temmieayodele@gmail.com>
2020-08-01 14:49:07 +0100

    lightbox

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const cardButton = document.querySelector('.card__checkfood')
 const modal = document.querySelector('.modal')
-const closeModalBtn = document.querySelector('.modal__close')
+const close = document.querySelectorAll(".close-modal")
+const modalBody = document.querySelector('.modal__body')
+const lightBox = document.querySelector(".lightbox")
+const body = document.body
 
 const openModal = () => {
   modal.style.display = 'block'
+  body.classList.add('no-scroll')
 }
 const closeModal = () => {
-  modal.style.display = 'none'
+  if (modal.style.display === 'block') {
+    modal.style.display = 'none'
+    body.classList.remove('no-scroll')
+  }
 }
 cardButton.addEventListener('click', openModal)
-closeModalBtn.addEventListener('click', closeModal)
+
+modalBody.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+lightBox.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+
+close.forEach(item => {
+  item.addEventListener('click', closeModal)
+})
